b5b1742625a06d5965bd103c28ea4113131a510f
ejangpa <jbpvns@gmail.com>
2017-01-18 15:58:00 +0530

    add building MinHeap Program

--- src/com/hackerrank/java/MaximumElement.java
+++ src/com/hackerrank/java/MaximumElement.java
@@ -13,8 +13,8 @@ import java.io.*;
 
 public class MaximumElement {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+ /*   public static void main(String[] args) {
+        *//* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. *//*
         Stack stack = new Stack();
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
@@ -30,9 +30,41 @@ public class MaximumElement {
                 System.out.println(max);
             }
         }
+    }*/
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Stack stack = new Stack();
+        ArrayList<Long> al = new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        long max = 0;
+        for (int i = 0; i < N; i++) {
+            int choice = scanner.nextInt();
+            if(choice == 1) {
+                long element = scanner.nextLong();
+                if (element > max) {
+                    max = element;
+                }
+                al.add(max);
+                stack = pushToStack(element, stack);
+            } else if ( choice == 2){
+                stack = deleteStackElement(stack);
+                al.remove(al.size() - 1);
+                if (al.size() == 0) {
+                    max = 0;
+                } else {
+                    max = al.get(al.size() - 1);
+                }
+                System.out.println(al.size());
+            } else if ( choice == 3 ) {
+                // stack  = findMaxElementInStack(stack);
+                System.out.println(al.get(al.size() - 1));
+            }
+        }
     }
 
-    static Stack pushToStack(int element, Stack stack) {
+    static Stack pushToStack(long element, Stack stack) {
         stack.push(element);
         return stack;
     }
